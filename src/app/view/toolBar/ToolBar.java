77b57b0cd1c97a44fa15d831dd2f4758722ac6e7
anguzuDaniel <102733205+anguzuDaniel@users.noreply.github.com>
2023-01-08 14:57:17 +0300

    added tool bar icons
    added icons for the save and refresh btn

--- src/app/view/toolBar/ToolBar.java
+++ src/app/view/toolBar/ToolBar.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
 public class ToolBar extends JPanel implements ActionListener {
 
@@ -19,15 +20,30 @@ public class ToolBar extends JPanel implements ActionListener {
 
         saveBtn = new JButton("Save");
         saveBtn.setPreferredSize(toolBarBtnDim);
+        saveBtn.setIcon(createIcon("/images/Save16.gif"));
 
 
         saveBtn.addActionListener(this);
         refreshBtn.addActionListener(this);
+        refreshBtn.setIcon(createIcon("/images/Refresh16.gif"));
 
         setLayout(new FlowLayout(FlowLayout.LEFT));
 
         add(saveBtn);
         add(refreshBtn);
+    }
+
+
+    private ImageIcon createIcon(String path) {
+        URL url = getClass().getResource(path);
+
+        if (url == null) {
+            System.err.println("Unable to find Icon image provided ");
+        }
+
+        ImageIcon icon = new ImageIcon(url);
+
+        return icon;
     }
 
     public void setToolBarListener(ToolBarListener listener) {
